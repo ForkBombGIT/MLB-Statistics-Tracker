766ec9ff5c9127e822eb856ca49ab1320bc5b2d2
ForkBombGIT <erosdipede@gmail.com>
2019-06-18 01:25:32 -0700

    WIP Fix data.frame building

--- MLBStatisticsTracker/server.R
+++ MLBStatisticsTracker/server.R
@@ -7,108 +7,145 @@ library(hash)
 mlb_url <- "http://mlb.mlb.com/stats/sortable.jsp#elem=%5Bobject+Object%5D&tab_level=child&click_text=Sortable+Player+ROLE&game_type='R'&season=YEAR&season_type=ANY&league_code='MLB'&sectionType=sp&statType=ROLE&page=1&ts=1560404713148"
 
 #hash to convert input to html tags
-input.to.tags <- hash(c("G (Games Played)",
-                      "AB (At Bats)",
-                      "R (Runs)",
-                      "H (Hits)",
-                      "2B (Doubles)",
-                      "3B (Triples)",
-                      "HR (Homeruns)",
-                      "RBI (Runs Batted In)",
-                      "BB (Base on Balls)",
-                      "SO (Strikeouts)",
-                      "SB (Stolen Bases)",
-                      "CS (Caught Stealing)",
-                      "AVG (Batting Average)",
-                      "OBP (On Base Percentage)",
-                      "SLG (Slugging Percentage)",
-                      "OPS (OBS + SLG)",
-                      "W (Wins)",
-                      "L (Losses)",
-                      "ERA (Earned Runs Average)",
-                      "G (Game Appearances)",
-                      "GS (Games Started)",
-                      "SV (Saves)",
-                      "SV (Save Appearances)",
-                      "IP (Innings Pitches)",
-                      "H (Hits Allowed)",
-                      "R (Runs Allowed)",
-                      "ER (Earned Runs Allowed)",
-                      "HR (Homeruns Allowed)",
-                      "BB (Walks Allowed)",
-                      "SO (Strikeouts)",
-                      "AVG (H/AB)",
-                      "WHIP (AVG of Walks + Hits Per IP)"),
-                    c("dg-g",
-                      "dg-ab",
-                      "dg-r",
-                      "dg-h",
-                      "dg-d",
-                      "dg-t",
-                      "dg-hr",
-                      "dg-rbi",
-                      "dg-bb",
-                      "dg-so",
-                      "dg-sb",
-                      "dg-cs",
-                      "dg-avg",
-                      "dg-obp",
-                      "dg-slg",
-                      "dg-ops",
-                      "dg-w",
-                      "dg-l",
-                      "dg-era",
-                      "dg-g",
-                      "dg-gs",
-                      "dg-sv",
-                      "dg-svo",
-                      "dg-ip",
-                      "dg-h",
-                      "dg-r",
-                      "dg-er",
-                      "dg-hr",
-                      "dg-bb",
-                      "dg-so",
-                      "dg-avg",
-                      "dg-whip"))
-                      
+input_hash <- hash(c("G (Games Played)",
+                     "AB (At Bats)",
+                     "R (Runs)",
+                     "H (Hits)",
+                     "2B (Doubles)",
+                     "3B (Triples)",
+                     "HR (Homeruns)",
+                     "RBI (Runs Batted In)",
+                     "BB (Base on Balls)",
+                     "SO (Strikeouts)",
+                     "SB (Stolen Bases)",
+                     "CS (Caught Stealing)",
+                     "AVG (Batting Average)",
+                     "OBP (On Base Percentage)",
+                     "SLG (Slugging Percentage)",
+                     "OPS (OBS + SLG)",
+                     "W (Wins)",
+                     "L (Losses)",
+                     "ERA (Earned Runs Average)",
+                     "G (Game Appearances)",
+                     "GS (Games Started)",
+                     "SV (Saves)",
+                     "SV (Save Appearances)",
+                     "IP (Innings Pitches)",
+                     "H (Hits Allowed)",
+                     "R (Runs Allowed)",
+                     "ER (Earned Runs Allowed)",
+                     "HR (Homeruns Allowed)",
+                     "BB (Walks Allowed)",
+                     "SO (Strikeouts)",
+                     "AVG (H/AB)",
+                     "WHIP (AVG of Walks + Hits Per IP)"),
+                   c("dg-g",
+                     "dg-ab",
+                     "dg-r",
+                     "dg-h",
+                     "dg-d",
+                     "dg-t",
+                     "dg-hr",
+                     "dg-rbi",
+                     "dg-bb",
+                     "dg-so",
+                     "dg-sb",
+                     "dg-cs",
+                     "dg-avg",
+                     "dg-obp",
+                     "dg-slg",
+                     "dg-ops",
+                     "dg-w",
+                     "dg-l",
+                     "dg-era",
+                     "dg-g",
+                     "dg-gs",
+                     "dg-sv",
+                     "dg-svo",
+                     "dg-ip",
+                     "dg-h",
+                     "dg-r",
+                     "dg-er",
+                     "dg-hr",
+                     "dg-bb",
+                     "dg-so",
+                     "dg-avg",
+                     "dg-whip"))
+
+hitting_choices <- list("G (Games Played)",
+                        "AB (At Bats)",
+                        "R (Runs)",
+                        "H (Hits)",
+                        "2B (Doubles)",
+                        "3B (Triples)",
+                        "HR (Homeruns)",
+                        "RBI (Runs Batted In)",
+                        "BB (Base on Balls)",
+                        "SO (Strikeouts)",
+                        "SB (Stolen Bases)",
+                        "CS (Caught Stealing)",
+                        "AVG (Batting Average)",
+                        "OBP (On Base Percentage)",
+                        "SLG (Slugging Percentage)",
+                        "OPS (OBS + SLG)")
+
+pitching_choices <- list("W (Wins)",
+                         "L (Losses)",
+                         "ERA (Earned Runs Average)",
+                         "G (Game Appearances)",
+                         "GS (Games Started)",
+                         "SV (Saves)",
+                         "SV (Save Appearances)",
+                         "IP (Innings Pitches)",
+                         "H (Hits Allowed)",
+                         "R (Runs Allowed)",
+                         "ER (Earned Runs Allowed)",
+                         "HR (Homeruns Allowed)",
+                         "BB (Walks Allowed)",
+                         "SO (Strikeouts)",
+                         "AVG (H/AB)",
+                         "WHIP (AVG of Walks + Hits Per IP)")
+
+
+hitting_stats <- list(".dg-g",
+                      ".dg-ab",
+                      ".dg-r",
+                      ".dg-h",
+                      ".dg-d",
+                      ".dg-t",
+                      ".dg-hr",
+                      ".dg-rbi",
+                      ".dg-bb",
+                      ".dg-so",
+                      ".dg-sb",
+                      ".dg-cs",
+                      ".dg-avg",
+                      ".dg-obp",
+                      ".dg-slg")
+
+pitching_stats <- list(".dg-w",
+                       ".dg-l",
+                       ".dg-era",
+                       ".dg-g",
+                       ".dg-gs",
+                       ".dg-sv",
+                       ".dg-svo",
+                       ".dg-ip",
+                       ".dg-h",
+                       ".dg-r",
+                       ".dg-er",
+                       ".dg-hr",
+                       ".dg-bb",
+                       ".dg-so",
+                       ".dg-avg",
+                       ".dg-whip")
 
 #retieves data from mlb website
 get.data <- function(input) {
   #Convert url
   url <- mlb_url %>% gsub("YEAR",input$year,.) %>% gsub("ROLE",tolower(input$role),.)
-  hitting_stats <- list(".dg-g",
-                        ".dg-ab",
-                        ".dg-r",
-                        ".dg-h",
-                        ".dg-d",
-                        ".dg-t",
-                        ".dg-hr",
-                        ".dg-rbi",
-                        ".dg-bb",
-                        ".dg-so",
-                        ".dg-sb",
-                        ".dg-cs",
-                        ".dg-avg",
-                        ".dg-obp",
-                        ".dg-slg")
  
-  pitching_stats <- list(".dg-w",
-                         ".dg-l",
-                         ".dg-era",
-                         ".dg-g",
-                         ".dg-gs",
-                         ".dg-sv",
-                         ".dg-svo",
-                         ".dg-ip",
-                         ".dg-h",
-                         ".dg-r",
-                         ".dg-er",
-                         ".dg-hr",
-                         ".dg-bb",
-                         ".dg-so",
-                         ".dg-avg",
-                         ".dg-whip")
+ 
   
   # Start the Server
   rD <- rsDriver(browser = "firefox")
@@ -125,60 +162,26 @@ get.data <- function(input) {
                   gsub("[\r\n]","",.) %>%
                   factor()
   
-  df <- data.frame("names" = player_names)
-  
   #iterate through possible options, name the columns after the html class
-  list <- if (input$role == "Hitting") hitting_stats else pitching_stats
-  datalist <- list()
+  df <- data.frame("names" = player_names)
+  list <- if (input$role == "Hitting") hitting_stats else pitching_stat
+  frame_list <- list(df)
   for (i in 1:length(list)) {
     col <- source %>% 
            html_nodes(list[[i]]) %>% 
            html_text() %>%  
            gsub("[\r\n]","",.)
     
-    col_name <- list[[i]]
-    datalist[[i]] <- data.frame(col_name = col)
+    tdf <- data.frame(x = col, "names" = player_names)
+    names(tdf) <- c(gsub("[.]","",list[[i]]))
+    df <- rbind(df,tdf)
   }
-   
-  datalist         
+  df         
 }
 
 # Define server logic 
 server <- function(input, output,session) {
     observe({
-      hitting_choices <- list("G (Games Played)",
-                             "AB (At Bats)",
-                             "R (Runs)",
-                             "H (Hits)",
-                             "2B (Doubles)",
-                             "3B (Triples)",
-                             "HR (Homeruns)",
-                             "RBI (Runs Batted In)",
-                             "BB (Base on Balls)",
-                             "SO (Strikeouts)",
-                             "SB (Stolen Bases)",
-                             "CS (Caught Stealing)",
-                             "AVG (Batting Average)",
-                             "OBP (On Base Percentage)",
-                             "SLG (Slugging Percentage)",
-                             "OPS (OBS + SLG)")
-      pitching_choices <- list("W (Wins)",
-                              "L (Losses)",
-                              "ERA (Earned Runs Average)",
-                              "G (Game Appearances)",
-                              "GS (Games Started)",
-                              "SV (Saves)",
-                              "SV (Save Appearances)",
-                              "IP (Innings Pitches)",
-                              "H (Hits Allowed)",
-                              "R (Runs Allowed)",
-                              "ER (Earned Runs Allowed)",
-                              "HR (Homeruns Allowed)",
-                              "BB (Walks Allowed)",
-                              "SO (Strikeouts)",
-                              "AVG (H/AB)",
-                              "WHIP (AVG of Walks + Hits Per IP)")
-      
       updateSelectInput(session,"x",choices = if (input$role == "Hitting") hitting_choices else pitching_choices)
       updateSelectInput(session,"y",choices = if (input$role == "Hitting") hitting_choices else pitching_choices)
     })
